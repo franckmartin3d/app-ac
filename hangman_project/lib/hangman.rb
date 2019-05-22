68899b49c917daa181fa79558b06e473dd900b2c
franckmartin3d <franck.martin3d@gmail.com>
2019-05-23 07:36:29 +1000

    Hotel

--- hangman_project/lib/hangman.rb
+++ hangman_project/lib/hangman.rb
@@ -32,13 +32,89 @@ class Hangman
     end
 
   end
+
   def get_matching_indices(char)
     new_arr = []
+    working_arr = @secret_word.split("")
+    #iterate over @ secret word
+    working_arr.each_with_index do |ele, idx|
+      if ele == char
+        new_arr << idx
+      end
+    end
+    return new_arr
+  end
 
+  def fill_indices(char,arr)
+    #set the giving indices to @guess_word
     
+    arr.each do |indices|
+
+      @guess_word[indices] = char
+    end
+
 
 
   end
+############################part 2####################################
+
+  def try_guess(char)
+    if self.already_attempted?(char)
+      p "that has already been attempted"
+      return false
+    else
+      @attempted_chars << char
+    
+
+      matches = self.get_matching_indices(char)
+      self.fill_indices(char,matches)
+
+      if matches.empty?
+        @remaining_incorrect_guesses -= 1
+      end
+
+      return true
+
+    end
+    
+  end
+
+  def ask_user_for_guess
+
+    puts "Enter a char:"
+    answer = gets.chomp
+
+    self.try_guess(answer)
+
+  end
+  def win?
+    if @guess_word.join("") == @secret_word
+      puts "WIN"
+      return true
+    else 
+      return false
+    end
+  end
+  
+  def lose?
+    if @remaining_incorrect_guesses <= 0
+      puts "LOSE"
+      return true
+    else
+      return false
+    end 
+  end
+  def game_over?
+    if self.win? || self.lose?
+     puts @secret_word
+     return true
+    else
+      return false
+    end
+  end
+
+  
+
 
 
 end
---
+++ hotel_project/lib/hotel.rb
@@ -0,0 +1,47 @@
+require_relative "room"
+
+class Hotel
+    def initialize(name,capacities)
+        @name = name 
+        @rooms = {}
+
+        capacities.each do |room_name, capacity|
+            @rooms[room_name] = Room.new(capacity)
+        end
+
+    end
+
+    def name
+       cap =  @name.split(" ")
+       cap.each {|ele| ele.capitalize!}
+       name = cap.join(" ")
+    end
+
+    def rooms
+        @rooms
+    end
+
+    def room_exists?(name)
+        rooms.each_key do |key|
+            if key == name
+                return true
+            end
+            return false
+
+        end
+    end
+
+    def check_in(person, room)
+        if self.room_exists?(room)
+            @rooms[room].add_occupant(person)
+        else
+            puts "sorry, room does not exist"
+        end
+    end
+
+    def has_vacancy?
+        
+
+    end
+    
+end
